b000ba39864c52dcabb4c0671bb520d448f9ce1c
Thomas <thomasit27@gmail.com>
2025-06-07 22:52:22 +0530

    poular destination ui changs

---
+++ patterns/popular-destinations.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Title: Popular Destinations
+ * Slug: yourplatform/popular-destinations
+ * Categories: featured, travel
+ * Keywords: travel, destinations, featured
+ */
+
+?>
+
+<!-- wp:group {"className":"featured-category-section light-color","layout":{"type":"constrained"}} -->
+<div class="wp-block-group section-paading featured-category-section popular_destination_section d-flex flex-column flex-nowrap align-items-center gap-40">
+  <!-- wp:heading {"level":5,"className":"section-title"} -->
+  <h5 class="section-title">Popular Destinations</h5>
+  <!-- /wp:heading -->
+
+  <!-- wp:columns {"className":"popular-destinations"} -->
+  <div class="wp-block-columns popular-destinations">
+    <div class="wp-block-column">
+      <div class="">
+        <span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
+        <div class="wp-block-cover__inner-container">
+          <a href="https://demo.loftocean.com/eaven-travel/category/destinations/australia/">
+            <p class="has-text-align-center"><strong>Destination 01</strong></p>
+          </a>
+        </div>
+      </div>
+      <!-- /wp:cover -->
+    </div>
+
+        <div class="wp-block-column">
+        <div class="">
+        <span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
+        <div class="wp-block-cover__inner-container">
+            <a href="https://demo.loftocean.com/eaven-travel/category/destinations/australia/">
+            <p class="has-text-align-center"><strong>Destination 02</strong></p>
+            </a>
+        </div>
+        </div>
+        <!-- /wp:cover -->
+    </div>
+
+        <div class="wp-block-column">
+        <div class="">
+        <span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
+        <div class="wp-block-cover__inner-container">
+            <a href="https://demo.loftocean.com/eaven-travel/category/destinations/australia/">
+            <p class="has-text-align-center"><strong>Destination 03</strong></p>
+            </a>
+        </div>
+        </div>
+        <!-- /wp:cover -->
+    </div>
+
+  </div>
+  <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
