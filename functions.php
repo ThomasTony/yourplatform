8bdac62e01f81423f8091eb79a27dcb98290e647
Thomas <thomasit27@gmail.com>
2025-06-09 11:01:19 +0530

    website pages added

--- functions.php
+++ functions.php
@@ -4,11 +4,7 @@ require_once get_template_directory() . '/inc/ThemeSetup.php';
 require_once get_template_directory() . '/inc/ThemeAssets.php';  
 require_once get_template_directory() . '/inc/ThemeOptions.php';
 
-add_theme_support('block-patterns');
-
 // Initialize singleton instance
 ThemeSetup::instance();
 ThemeOptions::instance();
 ThemeAssets::get_instance();
-
-
